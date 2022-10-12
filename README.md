6c3ff67194422066484d642b5aaaf3307c509c3f
mosman7 <mohamedosman998@hotmail.com>
2022-10-12 14:19:13 +0100

    push

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# python control flow
+# Python control flow
+## If elif else
+### Loops - for loop, while loop
+
+psuedo code
+
---
+++ control_flow.py
@@ -0,0 +1,9 @@
+
+weather = "raining"
+
+if weather == "cold":       #condition - depending on whether true or false, prints value
+    print("wear a jacket")
+elif weather == "raining":
+    print("take an umbrella")
+else:                       # if none of the conditions are met, this will be executed
+    print("dont")
